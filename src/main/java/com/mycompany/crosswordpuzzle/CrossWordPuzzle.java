297c486c035810bc77398dd4abce79f754e14972
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2023-07-21 07:39:08 +0100

    Added individual files to the project

---
+++ src/main/java/com/mycompany/crosswordpuzzle/CrossWordPuzzle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crosswordpuzzle;
+
+/**
+ *
+ * @author user
+ */
+public class CrossWordPuzzle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
